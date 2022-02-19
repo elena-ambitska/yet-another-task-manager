6e47628ea082dd6512388e203566119886037cde
Lena <lambitska92@gmail.com>
2022-02-19 09:02:30 +0200

    Rename  cards and change JSON structure

--- src/tests/mock/cards_list.js
+++ src/tests/mock/cards_list.js
@@ -1,56 +1,56 @@
 export default [
     {
         "id": 1129,
-        "title": "Olena is the Best 1",
-        "description": "Do you have any doubt?",
+        "title": "Header",
+        "description": "Just do it",
         "status": "in_progress",
         "published_at": "2022-02-18T21:55:09.676Z",
         "created_at": "2022-02-18T21:55:09.686Z",
-        "updated_at": "2022-02-18T21:55:09.696Z"
+        "updated_at": "2022-02-19T06:52:16.964Z"
     },
     {
         "id": 1130,
-        "title": "Olena is the Best 2",
-        "description": "Do you have any doubt?",
+        "title": "Component",
+        "description": "Just do it",
         "status": "in_progress",
         "published_at": "2022-02-18T21:55:18.840Z",
         "created_at": "2022-02-18T21:55:18.844Z",
-        "updated_at": "2022-02-18T21:55:18.849Z"
+        "updated_at": "2022-02-19T06:52:48.442Z"
     },
     {
         "id": 1131,
-        "title": "Olena is the Best 3",
-        "description": "Do you have any doubt?",
-        "status": "in_progress",
+        "title": "Learn React",
+        "description": "Just do it",
+        "status": "done",
         "published_at": "2022-02-18T21:55:24.761Z",
         "created_at": "2022-02-18T21:55:24.766Z",
-        "updated_at": "2022-02-18T21:55:24.771Z"
+        "updated_at": "2022-02-19T06:55:39.446Z"
     },
     {
         "id": 1132,
-        "title": "Olena is the Best 4",
-        "description": "Do you have any doubt?",
+        "title": "Learn JS",
+        "description": "Just do it",
         "status": "done",
         "published_at": "2022-02-18T21:55:53.564Z",
         "created_at": "2022-02-18T21:55:53.569Z",
-        "updated_at": "2022-02-18T21:55:53.575Z"
+        "updated_at": "2022-02-19T06:56:06.376Z"
     },
     {
         "id": 1133,
-        "title": "Olena is the Best 5",
-        "description": "Do you have any doubt?",
+        "title": "Learn JS",
+        "description": "Just do it",
         "status": "to_do",
         "published_at": "2022-02-18T21:57:33.446Z",
         "created_at": "2022-02-18T21:57:33.453Z",
-        "updated_at": "2022-02-18T21:57:33.460Z"
+        "updated_at": "2022-02-19T06:56:43.757Z"
     },
     {
         "id": 1134,
-        "title": "Olena is the Best 6",
+        "title": "Group 1",
         "description": "Do you have any doubt?",
-        "status": "testing",
+        "status": "to_do",
         "published_at": "2022-02-18T21:57:43.518Z",
         "created_at": "2022-02-18T21:57:43.521Z",
-        "updated_at": "2022-02-18T21:57:43.526Z"
+        "updated_at": "2022-02-19T06:57:27.704Z"
     }
-];
+]
